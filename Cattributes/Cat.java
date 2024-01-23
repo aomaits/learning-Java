f06107eeaf2f5b7df2b3e23bfce33cef0c6e2c31
Abe Omaits <aomaits@gmail.com>
2024-01-23 13:36:44 -0600

    Added the cat attributes file, but it doesn't run anything

---
+++ Cattributes/Cat.java
@@ -0,0 +1,38 @@
+package Cattributes;
+
+public class Cat {
+    // Private instance variables, encapsulating the state of the Cat object
+    private String name;
+    private int age;
+
+    // Constructor that initializes a Cat object with a given name and age
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Public getter for the name, allowing read access to the private variable
+    public String getName() {
+        return this.name;
+    }
+
+    // Public setter for the name, allowing write access to the private variable
+    public void setName( String name) {
+        this.name = name;
+    }
+
+    // Public getter for the age, allowing read access to the private variable
+    public int getAge() {
+        return this.age;
+    }
+
+    // Public setter for the age, allowing write access to the private variable
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Public method to display the Cat's attributes (Cattributes)
+    public void displayInfo() {
+        System.out.println(this.name + " is " + this.age + " year(s) old.");
+    }
+}
