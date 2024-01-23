80a241b69996d6a697cda2eca771a26066994102
Abe Omaits <aomaits@gmail.com>
2024-01-23 14:55:00 -0600

    Created MyClass file to note basic syntax. Chapter 1 finished

---
+++ MyClass.java
@@ -0,0 +1,15 @@
+// The 'public' modifier allows this class to be accessed from other classes.
+public class MyClass {
+    // 'main' method: Java starts executing the program from this method.
+    public static void main(String[] args) {
+        // ... code goes here
+    }
+    
+}
+
+// 1. CLASS DECLARATION
+    // The public keyword specifies that the class is accessible from anywhere in the program, promoting a modular and interactive coding environment.
+// 2. THE MAIN METHOD
+    // Program's execution commences here. Must be public to be universally invokable, static to be callable with instantiating the class, and void to indicate it doesn't return any value. The String[] args parameter serves as a container for any command-line arguments that may be passed to the program.
+// 3. FILE NAME
+    // The name of the source file should precisely match the class name and should have the .java extension. This is essential for the Java compiler to recognize and compile the class correctly. 
