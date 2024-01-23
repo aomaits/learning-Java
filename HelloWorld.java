1ebd0d569f249f61b0a4af0408d840d57e6bcebc
Abe Omaits <aomaits@gmail.com>
2024-01-23 12:52:23 -0600

    Hello world comments added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
+// HelloWorld is the name of the class
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) { //This is the doorway into the program
+        System.out.println("Hello, World!"); //Java's way of printing to the console
     }
 }
